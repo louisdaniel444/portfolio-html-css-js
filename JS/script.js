6b3a517f49b83e55d4a6e407f1be97817d58d6a4
louisdaniel444 <louisdaniel119@gmail.com>
2025-04-23 16:42:48 +0200

    ajout ouverture et fermeture du  menu mobile

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,42 @@
-let pseudo ="Da";
- pseudo += " whouaf";
- //console.log(pseudo);
+// <button class="btn-menu-mobile">
 
- const age =53;
- //console.log(age);
 
-direBonjour(pseudo,age);
+// const boutonMobile = document.querySelector (".btn-menu-mobile");
+// console.log (boutonMobile);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const boutonMobile = document.querySelector (".btn-menu-mobile");
+  console.log (boutonMobile);
+
+  const boutonCloseMobile = document.querySelector (".btn-close-menu-mobile");
+  console.log (boutonCloseMobile);
+
+  const menuMobile = document.querySelector (".menu-mobile");
+  console.log  (menuMobile);
+
+  const liensMobile = document.querySelectorAll(".menu-mobile a");
+  console.log (liensMobile)
+
+
+  function openMenu (){
+    console.log ('open menu est activé');
+    menuMobile.classList.add("open");
+    boutonCloseMobile.classList.add("visible");
+  }
+
+  function closeMenu () {
+    console.log ('close menu');
+    menuMobile.classList.remove("open");
+    boutonCloseMobile.classList.remove("visible");
+  }
+  
+  boutonMobile.addEventListener('click',openMenu);   
+  boutonCloseMobile.addEventListener('click',closeMenu);
+
+  liensMobile.forEach(lien =>{
+     lien.addEventListener('click',closeMenu);
+  }
+
+  )
  
-
-function direBonjour(pseudo,age){
-  const ageLimite=18; 
-  if(age>=ageLimite){
-   console.log("salut à toi " +pseudo+" tu as "+age);
-}else{
-  console.log("tu n,as pas l,age requis");
-}
- }
-
+});
